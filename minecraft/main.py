d48fbbab242dc7375fd4a4cd284c3aa13e35b110
Nova <purplecat9909@gmail.com>
2025-02-01 22:02:09 -0800

    test interface with TMUX minecraft server

--- minecraft/main.py
+++ minecraft/main.py
@@ -1,5 +1,6 @@
 from redbot.core import commands, bot as Bot
 from discord.ext.commands import Context
+import subprocess
 
 class Minecraft(commands.Cog):
     def __init__(self, bot: Bot): # noqa
@@ -7,4 +8,5 @@ class Minecraft(commands.Cog):
 
     @commands.command()
     async def ping(self, ctx: Context):
+        subprocess.run(["tmux", "send-keys", "-t", "CM", "\"say", "meow\"", "enter"])
         await ctx.send('Pong!')
